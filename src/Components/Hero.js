407cf096dfe0a72fe4deeab3b39184c31c589f38
NDICHU <ndichumuriithi@gmail.com>
2022-05-25 03:24:48 +0300

    Initial commit

---
+++ src/Components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {  } from "../index.css";
+
+function Hero() {
+  return (
+    <div>
+        
+        <div className="bg"></div>
+    </div>
+  );
+}
+
+export default Hero;
